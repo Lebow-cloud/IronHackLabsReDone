3bb0b2490b3eab43099bc924e1cbb1b877fa6070
Daniel Milan <danielmilanvera@gmail.com>
2022-01-22 16:32:30 +0100

    Solution re-lad 04

--- src/movies.js
+++ src/movies.js
@@ -29,35 +29,97 @@ function howManyMovies(movies) {
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(movies) {
-
   if (!movies.length) return 0;
 
-  const scoresAvg = movies.reduce((acc,score)=> {
-    return acc += score.score},0)
+  const scoresAvg = movies.reduce((acc, score) => {
+    if (score.score) {
+      return (acc += score.score);
+    } else {
+      return acc;
+    }
+  }, 0);
 
-  return Math.floor(scoresAvg/movies.length)
-    
+  const finalScore = scoresAvg / movies.length;
 
+  return Math.round(finalScore * 100) / 100;
 }
-
 
 // console.log(scoresAverage(movies))
 // console.log(scoresAverage(movies))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+
+function dramaMoviesScore(movies) {
+  return scoresAverage(
+    movies.filter((movi) => {
+      return movi.genre.includes('Drama');
+    })
+  );
+}
+
+// console.log(dramaMoviesScore(movies))
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  const copyOfMovies = movies.map((movi) => movi);
+  const sortedByYear = copyOfMovies.sort((ele1, ele2) => {
+    if (ele1.year === ele2.year) return ele1.title.localeCompare(ele2.title);
+    else return ele1.year - ele2.year;
+  });
+  return sortedByYear;
+}
+
+// console.log(orderByYear(movies));
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function orderAlphabetically(movies) {
+  const copyOfMovies2 = movies.map((movi) => movi.title);
+  return copyOfMovies2
+  .sort()
+  .slice(0, 20);
+}
+
+// console.log(orderAlphabetically(movies))
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+function turnHoursToMinutes(movies) {
+  
+  }
+
+
+
+
+// console.log(convertHours(movies));
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+function bestYearAvg(movies) {
+
+  if(!movies.length) return null
+
+  let masterObject = {}
+
+  movies.forEach(movie => {
+    if(!masterObject[movie.year]){
+      masterObject[movie.year] = [movie]
+    }else {
+      masterObject[movie.year].push(movie)
+    }
+
+  });
+
+  let highest = 0
+  let actualYear ;
+
+  for(let i in masterObject){
+    if(scoresAverage(masterObject[i]) > highest) {
+      highest = scoresAverage(masterObject[i])
+      actualYear = i;
+    }
+  }
+  return `The best year was ${actualYear} with an average score of ${highest}`;
+}
+
+console.log(bestYearAvg(movies))
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
