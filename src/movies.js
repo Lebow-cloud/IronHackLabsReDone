335e4f19d84d63cd169ee916a6d6633db93b7659
Daniel Milan <danielmilanvera@gmail.com>
2022-01-22 14:31:20 +0100

    Finish Iteration 3

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,48 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+
+const movies = require('./data');
+
+function getAllDirectors(movies) {
+  const directorsArr = movies.map((movie) => {
+    return movie.director;
+  });
+
+  return directorsArr;
+}
+
+// console.log(getAllDirectors(movies))
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  if (!movies.length) return 0;
+
+  const dramaArr = movies.filter((dir) => {
+    return dir.director === 'Steven Spielberg' && dir.genre.includes('Drama');
+  }).length;
+
+  return dramaArr;
+}
+
+// console.log(howManyMovies(movies))
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+
+  if (!movies.length) return 0;
+
+  const scoresAvg = movies.reduce((acc,score)=> {
+    return acc += score.score},0)
+
+  return Math.floor(scoresAvg/movies.length)
+    
+
+}
+
+
+// console.log(scoresAverage(movies))
+// console.log(scoresAverage(movies))
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
@@ -24,8 +59,6 @@ function turnHoursToMinutes() {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -37,6 +70,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
